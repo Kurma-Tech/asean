f0f93f3e54318946b31a108aa1d3f68b1a4d1fe2
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-09-14 18:20:07 +0545

    Map Filters

--- app/Http/Livewire/Client/HomeComponent.php
+++ app/Http/Livewire/Client/HomeComponent.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Livewire\Client;
 
+use App\Models\Country;
 use App\Models\Business;
+use App\Models\Patent;
 use App\Models\IndustryClassification;
 use Illuminate\Support\Facades\Log;
 use Livewire\Component;
@@ -15,18 +17,46 @@ class HomeComponent extends Component
     public $search = '';
     public $parent_id;
     public $industryClass;
-    public $type = 'Brunei';
+    public $country;
+    public $results;
+    public $classification;
+    public $type = "all";
     public $per_page = 10;
     private $filters = [];
     private $filters_all = [];
 
+    protected $listeners = ['resultsUpdated' => 'updatedResults'];
+
+    public function mount(){
+        $total_business_count = Business::count();
+        $total_patent_count = Patent::count();
+        $this->results = $total_business_count + $total_patent_count;
+    }
+
+    public function updatedResults($results)
+    {
+        $this->results  = $results;
+    }
+
+    public function updatedCountry($country)
+    {
+        $this->country = $country;
+        $this->emit("loader_on");
+        $this->emit('country_updated', $country);
+    }
 
     public function updatedType($type)
     {
         $this->type = $type;
-        Log::info($type);
+        $this->emit("loader_on");
         $this->emit('type_updated', $type);
-        
+    }
+
+    public function updatedClassification($classification)
+    {
+        $this->classification = $classification;
+        $this->emit("loader_on");
+        $this->emit('classification_updated', $classification);
     }
     
     public function render()
@@ -49,10 +79,18 @@ class HomeComponent extends Component
         $this->emit('updateMap', $this->search);
         $this->emit('updateReport');
 
+        $countries = Country::where("status", "1")->get();
+        $classifications = [];
+        if($this->type == "business"){
+            $classifications = IndustryClassification::get();
+        }
+
         return view('livewire.client.home-component', [
             'filters' => $this->filters,
             'filters_all' => $this->filters_all,
             'filter' => $filter,
+            'countries' => $countries,
+            'classifications' => $classifications,
             // 'type' => $this->type,
             'industryClass' => $this->industryClass
         ])->layout('layouts.client');
--- resources/views/livewire/client/home-component.blade.php
+++ resources/views/livewire/client/home-component.blade.php
@@ -1,99 +1,12 @@
 @push('extra-styles')
-    <link rel="stylesheet" href="{{ asset('client/dist/css/apexcharts.css') }}">
     <style>
-        #chart {
-            max-width: 650px;
-            margin: 15px 0;
-        }
-        .overflow-control { overflow: hidden; }
-        .map-overlay-box {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            height: 100%;
-            overflow-y: auto;
-            min-width: 450px;
-            padding: 15px 15px 20px;
-            background-color: rgba(0,0,0,.3);
-            z-index: 9;
-            backdrop-filter: blur(5px);
-            -webkit-backdrop-filter: blur(5px);
-        }
-        #map-overlay-scroll::-webkit-scrollbar-track
-        {
-            background-color: hsl(0deg 0% 2%);
-        }
-
-        #map-overlay-scroll::-webkit-scrollbar
-        {
-            width: 6px;
-            background-color: #F5F5F5;
-        }
-
-        #map-overlay-scroll::-webkit-scrollbar-thumb
-        {
-            -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.1);
-            background-color: hsl(18deg 3% 6%);;
-        }
-
-        .map-overlay-box > .overlay-title { font-size: 1.5em; line-height: 1em; font-weight: 600; margin: 0; padding: 0; margin-bottom: .3em; }
-        .map-overlay-box > .data-report-title { font-size: 1em; line-break: 1em; font-weight: 500; margin: 0; padding: 0; margin-bottom: .3em; }
-        .map-overlay-box > .data-report-count { font-size: 1em; line-break: 1em; font-weight: 400; margin: 0; padding: 0; margin-bottom: .3em; }
-
-
-        #filter-wrapper {
-            z-index: 99;
-            position: absolute;
-            right: 0;
-            top: 0;
-            bottom: 0;
-            min-width: 300px;
-            height: 100%;
-            transform: translateX(300px);
-            overflow-y: auto;
-            -webkit-transition: all 0.4s ease 0s;
-            -moz-transition: all 0.4s ease 0s;
-            -ms-transition: all 0.4s ease 0s;
-            -o-transition: all 0.4s ease 0s;
-            transition: all 0.4s ease 0s;
-            background-color: rgba(0,0,0,.3);
-            backdrop-filter: blur(5px);
-            padding: 15px;
-        }
-        
-        .filter-nav {
-            position: absolute;
-            top: 0;
-            width: 250px;
-            margin: 0;
-            padding: 0;
-            list-style: none;
-        }
-        
-        #filter-toggle {
-            z-index: 1;
-            position: absolute;
-            top: 15px;
-            right: 15px;
-        }
-        
-        #filter-wrapper.active {
-            right: 300px;
-            width: 300px;
-            -webkit-transition: all 0.4s ease 0s;
-            -moz-transition: all 0.4s ease 0s;
-            -ms-transition: all 0.4s ease 0s;
-            -o-transition: all 0.4s ease 0s;
-            transition: all 0.4s ease 0s;
-        }
-        .square { display: inline-block; padding: 6px; text-align: right; color: hsl(340deg 82% 52%);}
-        .square:hover { color: hsl(340deg 72% 42%)!important; }
-
-        .filter-inputs { margin-top: 20px; }
-
-        .apexcharts-tooltip,
-        .apexcharts-menu-item {
-            color: rgb(0, 0, 0);
+        .view-report {
+            cursor: pointer;
+            border-bottom: 1px dashed;
+            font-size: 17px;
+            line-height: 21px;
+            color: #e92063;
+            display: inline-block;
         }
     </style>
 @endpush
@@ -140,55 +53,55 @@
             <div class="container-fluid p-0">
                 <div class="row m-0">
                     <div class="col-12 col-sm-12 position-relative overflow-control p-0">
-                        @livewire('client.map.map-component', ['type' => $type])
+                        @livewire('client.map.map-component', ['type' => $type, 'country' => $country, 'classification' => $classification])
                         <div class="map-overlay-box" id="map-overlay-scroll">
                             <h1 class="overlay-title">Asean</h1>
                             <h3 class="data-report-title">All Countries</h3>
-                            <p class="data-report-count">1,419,71 results</p>
+                            <p class="data-report-count">{{$results}} results</p>
+                            <h3 class="view-report" id="view-report-element">Show Report</h3>
                             <hr class="mb-2">
                             <div id="chart"></div>
                         </div>
-                        <a id="filter-toggle" href="#" class="toggle square"><i class="fas fa-filter fa-lg" aria-hidden="true"></i></a>
+                        <a id="filter-toggle" href="#" class="toggle square"><i class="fas fa-filter fa-lg"
+                                aria-hidden="true"></i></a>
                         <div id="filter-wrapper" wire:ignore.self>
-                            <a id="close-filter" href="#" class="toggle square"><i class="fa fa-times fa-lg"></i></a>
+                            <a id="close-filter" href="#" class="toggle square"><i
+                                    class="fa fa-times fa-lg"></i></a>
                             <h5>Filter Options</h5>
                             <hr class="mb-2">
                             <div class="filter-inputs">
                                 <div class="form-group">
-                                    <label>Sort by Countries: {{$type}}</label>
-                                    <select class="form-control" style="width: 100%;" wire:model="type">
+                                    <label>Sort by Countries:</label>
+                                    <select class="form-control" style="width: 100%;" wire:model="country">
                                         <option hidden>Choose Countries</option>
-                                        <option value="Brunei">Brunei</option>
-                                        <option value="Cambodia">Cambodia</option>
-                                        <option value="Indonesia">Indonesia</option>
-                                        <option value="Laos">Laos</option>
-                                        <option value="Malaysia">Malaysia</option>
-                                        <option value="Myanmar">Myanmar</option>
-                                        <option value="Philippines">Philippines</option>
-                                        <option value="Singapore">Singapore</option>
-                                        <option value="Thailand">Thailand</option>
-                                        <option value="Vietnam">Vietnam</option>
+                                        @foreach ($countries as $country)
+                                            <option value="{{ $country->id }}">{{ $country->name }}</option>
+                                        @endforeach
                                     </select>
                                 </div>
                                 <div class="form-group">
                                     <label>Sort by Data:</label>
-                                    <select class="form-control" style="width: 100%;">
+                                    <select class="form-control" style="width: 100%;" wire:model="type">
                                         <option hidden>Choose Data Type</option>
-                                        <option value="Business">Business</option>
-                                        <option value="Patent">Patent</option>
-                                        <option value="Journals">Journals</option>
+                                        <option value="all">All</option>
+                                        <option value="business">Business</option>
+                                        <option value="patent">Patent</option>
+                                        <option value="journals">Journals</option>
                                     </select>
                                 </div>
-                                <div class="form-group">
-                                    <label>Sort by Classifications:</label>
-                                    <select class="form-control" style="width: 100%;">
-                                        <option hidden>Choose Classifications</option>
-                                        <option value="Classifications 1">Classifications 1</option>
-                                        <option value="Classifications 2">Classifications 2</option>
-                                        <option value="Classifications 3">Classifications 3</option>
-                                        <option value="Classifications 4">Classifications 4</option>
-                                    </select>
-                                </div>
+                                @if ($type != 'all')
+                                    <div class="form-group">
+                                        <label>Sort by Classifications:</label>
+                                        <select class="form-control" style="width: 100%;" wire:model="classification">
+                                            <option hidden>Choose Classifications</option>
+                                            <option value="">All</option>
+                                            @foreach ($classifications as $classification)
+                                                <option value="{{$classification->id}}">{{$classification->classifications}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                @endif
+
                             </div>
                         </div>
                     </div>
@@ -201,72 +114,72 @@
 @push('extra-scripts')
     <script src="{{ asset('client/dist/js/apexcharts.min.js') }}"></script>
     <script>
-        var options = {
-            series: 
-                [
-                    {
-                        name: "Business",
-                        data: [4400, 5500, 4100, 3000, 2200, 4300, 2100, 2160, 3300, 4400]
-                    }, 
-                    {
-                        name: "Patent",
-                        data: [5300, 3200, 3300, 5200, 1300, 6600, 3300, 5500, 4400, 3200]
-                    },
-                    {
-                        name: "Journals",
-                        data: [4600, 3600, 4100, 5400, 2200, 4300, 3300, 5500, 4400, 3200]
-                    }
-                ],
-                chart: {
-                    type: 'bar',
-                    height: 750,
-                    foreColor: '#fff',
+        var countryChartOption = {
+            series: [{
+                    name: "Business",
+                    data: [4400, 5500, 4100, 3000, 2200, 4300, 2100, 2160, 3300, 4400]
                 },
-                plotOptions: {
-                    bar: {
-                        horizontal: true,
-                        dataLabels: {
-                            position: 'top',
-                        },
-                    }
+                {
+                    name: "Patent",
+                    data: [5300, 3200, 3300, 5200, 1300, 6600, 3300, 5500, 4400, 3200]
                 },
-                dataLabels: {
-                    enabled: true,
-                    offsetX: -6,
-                    style: {
-                        fontSize: '11px',
-                        colors: ['#fff'],
-
-                    }
-                },
-                stroke: {
-                    show: true,
-                    width: 1,
-                    colors: ['#333']
-                },
-                tooltip: {
-                    shared: true,
-                    intersect: false
-                },
-                xaxis: {
-                    categories: ['Brunei', 'Cambodia', 'Indonesia', 'Laos', 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Vietnam'],
-                    colors: ['#fff']
-                },
-                colors: ['#ffd600', '#b71c1c', '#01579b'],
-                title: {
-                    text: "Showing result of 2022, All Categories, All Countries",
-                    align: 'left',
-                    margin: 0,
-                    offsetX: 0,
-                    offsetY: 0,
-                    floating: false,
-                    style: {
-                        fontSize:  '14px',
-                        fontWeight:  'bold',
-                        fontFamily:  undefined,
-                        color:  '#fff'
+                {
+                    name: "Journals",
+                    data: [4600, 3600, 4100, 5400, 2200, 4300, 3300, 5500, 4400, 3200]
+                }
+            ],
+            chart: {
+                type: 'bar',
+                height: 750,
+                foreColor: '#fff',
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true,
+                    dataLabels: {
+                        position: 'top',
                     },
                 }
+            },
+            dataLabels: {
+                enabled: true,
+                offsetX: -6,
+                style: {
+                    fontSize: '11px',
+                    colors: ['#fff'],
+
+                }
+            },
+            stroke: {
+                show: true,
+                width: 1,
+                colors: ['#333']
+            },
+            tooltip: {
+                shared: true,
+                intersect: false
+            },
+            xaxis: {
+                categories: ['Brunei', 'Cambodia', 'Indonesia', 'Laos', 'Malaysia', 'Myanmar', 'Philippines',
+                    'Singapore', 'Thailand', 'Vietnam'
+                ],
+                colors: ['#fff']
+            },
+            colors: ['#ffd600', '#b71c1c', '#01579b'],
+            title: {
+                text: "Showing result of 2022, All Categories, All Countries",
+                align: 'left',
+                margin: 0,
+                offsetX: 0,
+                offsetY: 0,
+                floating: false,
+                style: {
+                    fontSize: '14px',
+                    fontWeight: 'bold',
+                    fontFamily: undefined,
+                    color: '#fff'
+                },
+            }
         };
 
         var chart = new ApexCharts(document.querySelector("#chart"), options);
