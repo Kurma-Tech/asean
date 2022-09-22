74d7608d3d2a27d641b627bd5c871dd8c85f1d15
Sujan Pradhan <niweshs@gmail.com>
2022-09-22 17:00:06 +0545

    classification import resolve

--- app/Imports/ClassificationImport.php
+++ app/Imports/ClassificationImport.php
@@ -6,29 +6,35 @@ use App\Models\IndustryClassification;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 
 class ClassificationImport implements ToModel, WithHeadingRow, WithChunkReading, WithBatchInserts
 {
-    private $industryClassifications;
+    // private $industryClassifications;
 
     public function __construct()
     {
-        $this->industryClassifications = DB::table('industry_classifications')->select('id', 'classifications', 'parent_id')->where('parent_id', Null);
+        // $this->industryClassifications = DB::table('industry_classifications')->select('id')->where('parent_id', null);
     }
 
     public function model(array $row)
     {
-        if(isset($row['parent_name']))
-            $industryClassification = $this->industryClassifications
-                     ->where('classifications', $row['parent_name'])
-                     ->first();
-        else
-            $industryClassification = [];
 
+        if (isset($row['parent_name'])) {
+
+            $industryClassification = DB::table('industry_classifications')
+                ->select('id')
+                ->where('parent_id', null)
+                ->where('classifications', $row['parent_name'])
+                ->first();
+        } else {
+            $industryClassification = null;
+        }
+        Log:info(($industryClassification != null) ? $industryClassification->id : $row['parent_name']);
         return new IndustryClassification([
-            "parent_id"       => $industryClassification->id ?? null,
+            "parent_id"       => ($industryClassification != null) ? $industryClassification->id : null,
             "classifications" => $row['classifications'] ?? null,
             "psic_code"       => $row['psic_code'] ?? null,
         ]);
