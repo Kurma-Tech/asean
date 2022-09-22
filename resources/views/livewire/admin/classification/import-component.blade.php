1115b29226a63e0b27a4b6c23f76de73a6491d36
Sujan Pradhan <niweshs@gmail.com>
2022-09-22 15:11:41 +0545

    classification import update

--- resources/views/livewire/admin/classification/import-component.blade.php
+++ resources/views/livewire/admin/classification/import-component.blade.php
@@ -23,7 +23,7 @@
                         </blockquote>
                         <div class="mt-1 p-1"><strong class="text-red-400" style="font-size: 12px;display:block;">Colums accepted:</strong>
                             <code class="text-xs text-info-400">
-                                parent_id,
+                                parent_name,
                                 classifications,
                                 psic_code
                             </code>
