160eba2307e438fb4f8f2593c4f866eed8cfd3b3
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-09-30 06:41:01 +0545

    Reports and Bug Fixes

--- app/Http/Livewire/Client/Report/ReportComponent.php
+++ app/Http/Livewire/Client/Report/ReportComponent.php
@@ -4,12 +4,17 @@ namespace App\Http\Livewire\Client\Report;
 
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use App\Models\Country;
+use App\Models\IndustryClassification;
 
 class ReportComponent extends Component
 {
     public
         $chartBusinessCount,
-        $chartPatentsCount;
+        $chartPatentsCount,
+        $country,
+        $classification, 
+        $isFirstLoad = true;
 
     protected
         $business = [],
@@ -24,54 +29,143 @@ class ReportComponent extends Component
         $this->filterData();
     }
 
+    public function updatedCountry($country)
+    {
+        $this->country = $country;
+        $this->filterData();
+    }
+
+    public function updatedClassification($classification)
+    {
+        $this->countclassificationry = $classification;
+        $this->filterData();
+    }
+
     public function filterData()
     {
-        $businessQuery =  DB::table('businesses')->select('id', 'year');
+        $businessQuery =  DB::table('businesses')->select('id', 'year', 'date_registered', 'industry_classification_id');
         $patentQuery =  DB::table('patents')->select('id', 'date');
 
+
+        if ($this->country != null) {
+            if ($this->classification != null) {
+                $businessQuery = $businessQuery->where('country_id', $this->country)->where('industry_classification_id', $this->classification);
+            } else {
+                $businessQuery = $businessQuery->where('country_id', $this->country);
+            }
+        } else {
+            if ($this->classification != null) {
+                $businessQuery = $businessQuery->where('industry_classification_id', $this->classification);
+            }
+        }
+
         /* Get Query Data */
-        $this->business = $businessQuery->get();
-        $this->patents = $patentQuery->get();
+        $business = $businessQuery->get();
+        $patents = $patentQuery->get();
         /* Get Query Data End */
 
         /* Default data for Charts */
 
-        $this->chartBusinessCount = collect($this->business)->pluck('year')->countBy(); // business chart count
-        
-        $this->chartPatentsCount = collect($this->patents)->pluck('date')->countBy(function ($date) {
-            return substr(strchr($date, "/", 0), 4); 
-        }); // Count of filtered patents with year extraction
+        $this->chartBusinessCount = collect($business)->pluck('year')->countBy(); // business chart count
 
-        // dd($this->chartPatentsCount);
+        $emergingBusinessData = [];
+
+        $emergingBusiness = collect($business)->pluck('industry_classification_id')->countBy()->sortByDesc(null)->take(10);
+
+        foreach ($emergingBusiness as $key => $value) {
+            array_push($emergingBusinessData, [
+                "key" => IndustryClassification::find($key)->classifications,
+                "value" => $value
+            ]);
+        }
+
+        $this->chartPatentsCount = collect($patents)->pluck('date')->countBy(function ($date) {
+            return substr(strchr($date, "/", 0), 4);
+        }); // Count of filtered patents with year extraction
 
         /* Default data for Charts End*/
         $lineChartYears = $this->chartBusinessCount->keys()->concat($this->chartPatentsCount->keys())->unique();
         // dd($lineChartYears);
         $tempChartPatentsCount = [];
-        for ($i=0; $i < count($lineChartYears); $i++) { 
-            if($this->chartPatentsCount->has($lineChartYears[$i])){
+        for ($i = 0; $i < count($lineChartYears); $i++) {
+            if ($this->chartPatentsCount->has($lineChartYears[$i])) {
                 $tempChartPatentsCount[$lineChartYears[$i]] = $this->chartPatentsCount[$lineChartYears[$i]];
-            }else{
+            } else {
                 $tempChartPatentsCount[$lineChartYears[$i]] = null;
             }
         }
 
         $tempChartBusinessCount = [];
-        for ($i=0; $i < count($lineChartYears); $i++) { 
-            if($this->chartBusinessCount->has($lineChartYears[$i])){
+        for ($i = 0; $i < count($lineChartYears); $i++) {
+            if ($this->chartBusinessCount->has($lineChartYears[$i])) {
                 $tempChartBusinessCount[$lineChartYears[$i]] = $this->chartBusinessCount[$lineChartYears[$i]];
-            }else{
+            } else if ($lineChartYears[$i] == "" || $lineChartYears[$i] == null) {
+            } else {
                 $tempChartBusinessCount[$lineChartYears[$i]] = null;
             }
         }
 
-        $this->emit("reportsUpdated", ["businessCountByYears" => collect($tempChartBusinessCount)->values(), "patentCountByYears" => collect($tempChartPatentsCount)->values(), "lineChartYears" => collect($lineChartYears->sort())->values()]);
+        //forecast
+        // dd($tempChartBusinessCount);
+
+        $this->tempForcastData = $this->predict(collect($tempChartBusinessCount)->keys(), collect($tempChartBusinessCount)->values());
+
+        if ($this->isFirstLoad){
+            $this->emit("reportsFirstLoad", [
+                "businessCountByYears" => collect($tempChartBusinessCount)->values(),
+                "patentCountByYears" => collect($tempChartPatentsCount)->values(),
+                "lineChartYears" => collect($lineChartYears->sort())->values(),
+                "forecastedFrom" =>  $this->tempForcastData["forecastedDates"]->count() - collect($tempChartBusinessCount)->keys()->count(),
+                "forcastDates" => $this->tempForcastData["forecastedDates"],
+                "forcastData" => $this->tempForcastData["forecastedData"],
+                "emergingBusiness" => collect($emergingBusinessData)
+            ]);
+            $this->isFirstLoad = false;
+        }else{
+            $this->emit("reportsUpdated", [
+                "businessCountByYears" => collect($tempChartBusinessCount)->values(),
+                "patentCountByYears" => collect($tempChartPatentsCount)->values(),
+                "lineChartYears" => collect($lineChartYears->sort())->values(),
+                "forecastedFrom" =>  $this->tempForcastData["forecastedDates"]->count() - collect($tempChartBusinessCount)->keys()->count(),
+                "forcastDates" => $this->tempForcastData["forecastedDates"],
+                "forcastData" => $this->tempForcastData["forecastedData"],
+                "emergingBusiness" => collect($emergingBusinessData)
+            ]);
+        }
+
+    }
+
+    public function predict($forcastDates, $forcastData)
+    {
+        $nF = 0;
+        $nL = $forcastDates->last() - $forcastDates->first();
+        $pF = $forcastData->first();
+        $pL = $forcastData->last();
+        $last = $forcastDates->last();
+        $forecastedDates = $forcastDates;
+        $forecastedData = $forcastData;
+
+        if ($forcastDates->count() != 0 && $nL != 0) {
+            $r = pow($pL / $pF, 1 / $nL) - 1;
+
+            for ($i = 0; $i < 5; $i++) {
+                $forecastedDates->push($last + $i + 1);
+                $forecastedData->push(intval((pow(1 + $r, $nL + $i + 1)) * $pF));
+            }
+        }
+        // $p5 = (pow(1 + $r, 5)) * $pF;
+        // dd($forecastedData);
+        return ["forecastedDates" => $forecastedDates, "forecastedData" => $forecastedData];
     }
 
     public function render()
     {
         // dd(DB::table('businesses')->select('id', 'year')->pluck('year')->countBy());
+        $countries = Country::select('id', 'status', 'name')->where("status", "1")->get();
+        $classifications = IndustryClassification::select('id', 'classifications')->where('parent_id', '!=', null)->get();
         return view('livewire.client.report.report-component', [
+            'countries' => $countries,
+            'classifications' => $classifications,
             'allData' => DB::table('businesses')->select('id', 'year')->pluck('year')->countBy()
         ])->layout('layouts.client');
     }
