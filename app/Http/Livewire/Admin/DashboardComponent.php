420e51daf55e1a5831a923181d85c7d75fb1a55d
Sujan Pradhan <niweshs@gmail.com>
2022-10-16 10:33:22 +0545

    dashboard

--- app/Http/Livewire/Admin/DashboardComponent.php
+++ app/Http/Livewire/Admin/DashboardComponent.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Business;
+use App\Models\Journal;
+use App\Models\Patent;
+use App\Models\User;
 use Livewire\Component;
 
 class DashboardComponent extends Component
 {
+    public $total_member, $total_business, $total_patent, $total_journals;
+
     public function render()
     {
-        return view('livewire.admin.dashboard-component')->layout('layouts.admin');
+        $this->total_member   = User::count();
+        $this->total_business = Business::count();
+        $this->total_patent   = Patent::count();
+        $this->total_journals = Journal::count();
+        return view('livewire.admin.dashboard-component' , [
+            'total_member' => $this->total_member,
+            'total_business' => $this->total_business,
+            'total_patent' => $this->total_patent,
+            'total_journals' => $this->total_journals
+        ])->layout('layouts.admin');
     }
 }
