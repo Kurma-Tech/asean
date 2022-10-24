ab619d05f2dcc316edffb466b30b37d426c1b11f
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-10-25 04:45:03 +0545

    Update MapComponent.php

--- app/Http/Livewire/Client/Map/MapComponent.php
+++ app/Http/Livewire/Client/Map/MapComponent.php
@@ -90,7 +90,7 @@ class MapComponent extends Component
         /* Model Queries */
         DB::enableQueryLog();
         $businessQuery =  DB::table('businesses')->select('id', 'lat', 'long', 'year', 'company_name');
-        $patentQuery =  DB::table('patents')->select('id', 'lat', 'long', 'date', 'title');
+        $patentQuery =  DB::table('patents')->select('id', 'lat', 'long', 'registration_date', 'title');
         $journalQuery =  DB::table('journals')->select('id', 'lat', 'long', 'title');
         /* Model Queries End */
 
