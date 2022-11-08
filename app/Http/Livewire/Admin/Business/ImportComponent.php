6982231ffc513004cd5f276fd8d252a17ac83a9e
Sujan Pradhan <niweshs@gmail.com>
2022-11-08 15:59:22 +0545

    upload size and code change

--- app/Http/Livewire/Admin/Business/ImportComponent.php
+++ app/Http/Livewire/Admin/Business/ImportComponent.php
@@ -17,16 +17,19 @@ class ImportComponent extends Component
     public $error;
     public $success;
 
-    protected function rules()
+    public function updated($fields)
     {
-        return [
-            'file' => 'required|mimes:xlsx,xls,csv,txt|max:102400'
-        ];
+        $this->validateOnly($fields, [
+            'file' => 'required',
+        ]);
     }
 
     public function businessImport()
     {
-        $this->validate();
+        $this->validate([
+            'file' => 'required',
+        ]);
+
         ini_set('memory_limit', -1);
         DB::beginTransaction();
 
