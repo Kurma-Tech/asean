e7b841c762d4ec91c386b8dc8618b526f61d93d6
Sujan Pradhan <niweshs@gmail.com>
2022-11-08 11:14:42 +0545

    business

--- app/Imports/BusinessImport.php
+++ app/Imports/BusinessImport.php
@@ -13,10 +13,31 @@ class BusinessImport implements ToModel, WithHeadingRow, WithChunkReading, WithB
 {
     public function model(array $row)
     {
-        $businessType = DB::table('business_types')->select('id', 'type')->where('type', $row['business_type'])->first();
-        $industryClassification = DB::table('industry_classifications')->select('id', 'code')->where('code', $row['classification_code'])->first();
-        $country = DB::table('countries')->select('id', 'short_code')->where('short_code', $row['country_short_code'])->first();
-
+        if (isset($row['business_type'])) {
+            $businessType = DB::table('business_types')
+                ->select('id', 'type')
+                ->where('type', $row['business_type'])
+                ->first();
+        } else {
+            $businessType = null;
+        }
+        if (isset($row['classification_code'])) {
+            $industryClassification = DB::table('industry_classifications')
+                ->select('id', 'code')
+                ->where('code', $row['classification_code'])
+                ->first();
+        } else {
+            $industryClassification = null;
+        }
+        if (isset($row['country_short_code'])) {
+            $country = DB::table('countries')
+                ->select('id', 'short_code')
+                ->where('short_code', $row['country_short_code'])
+                ->first();
+        } else {
+            $country = null;
+        }
+        
         $date = explode('/', $row['date_registered']);
         
         return new Business([
@@ -24,16 +45,16 @@ class BusinessImport implements ToModel, WithHeadingRow, WithChunkReading, WithB
             "sec_no"                     => $row['sec_no'],
             "company_name"               => $row['company_name'],
             "date_registered"            => $row['date_registered'],
-            "business_type_id"           => $businessType->id ?? NULL,
-            "industry_classification_id" => $industryClassification->id ?? NULL,
-            "country_id"                 => $country->id ?? NULL,
-            "ngc_code"                   => $row['ngc_code'],
-            "status"                     => $row['status'],
-            "address"                    => $row['address'],
-            "industry_code"              => $row['industry_code'],
+            "business_type_id"           => ($businessType != null) ? $businessType->id : null,
+            "industry_classification_id" => ($industryClassification != null) ? $industryClassification->id : null,
+            "country_id"                 => ($country != null) ? $country->id : null,
+            "ngc_code"                   => $row['ngc_code'] ?? Null,
+            "status"                     => $row['status'] ?? 'Registered',
+            "address"                    => $row['address'] ?? Null,
+            "industry_code"              => $row['industry_code'] ?? Null,
             "industry_description"       => $row['industry_description'] ?? "",
-            "geo_code"                   => $row['geo_code'],
-            "geo_description"            => $row['geo_description'],
+            "geo_code"                   => $row['geo_code'] ?? Null,
+            "geo_description"            => $row['geo_description'] ?? Null,
             "lat"                        => $row['lat'],
             "long"                       => $row['long'],
             "month"                      => $date[0],
