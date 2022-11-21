d4382cb99ba157c6b259e2423cc1d6ba249ece66
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-11-21 19:42:27 +0545

    Update PatentListComponent.php

--- app/Http/Livewire/Admin/Patent/PatentListComponent.php
+++ app/Http/Livewire/Admin/Patent/PatentListComponent.php
@@ -82,7 +82,7 @@ class PatentListComponent extends Component
     public function mount()
     {
         $this->countries        = Country::select('id', 'name')->get();
-        $this->patentCategories = PatentCategory::where('group_id', '!=', Null)->select('ipc_code', 'classification_category')->limit(10);
+        $this->patentCategories = PatentCategory::where('group_id', '!=', Null)->select('ipc_code', 'classification_category')->take(10)->get();
         $this->patentKinds      = PatentKind::select('id', 'kind')->get();
         $this->patentTypes      = PatentType::select('id', 'type')->get();
     }
