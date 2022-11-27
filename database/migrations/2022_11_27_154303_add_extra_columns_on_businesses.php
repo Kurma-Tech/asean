f804a3024e74a4f7a99c20a9bace934816197948
Sujan Pradhan <niweshs@gmail.com>
2022-11-27 23:14:09 +0545

    migration update

--- database/migrations/2022_11_27_154303_add_extra_columns_on_businesses.php
+++ database/migrations/2022_11_27_154303_add_extra_columns_on_businesses.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,11 +15,11 @@ return new class extends Migration
     public function up()
     {
         Schema::table('businesses', function (Blueprint $table) {
-            $table->bigInteger('region_id')->nullable();
-            $table->bigInteger('province_id')->nullable();
-            $table->bigInteger('district_id')->nullable();
-            $table->bigInteger('city_id')->nullable();
-            $table->bigInteger('group_id')->nullable();
+            $table->bigInteger('region_id')->nullable()->unsigned();
+            $table->bigInteger('province_id')->nullable()->unsigned();
+            $table->bigInteger('district_id')->nullable()->unsigned();
+            $table->bigInteger('city_id')->nullable()->unsigned();
+            $table->bigInteger('group_id')->nullable()->unsigned();
 
             $table->foreign('region_id')->references('id')->on('regions')->onDelete('CASCADE')->onUpdate('CASCADE');
             $table->foreign('province_id')->references('id')->on('provinces')->onDelete('CASCADE')->onUpdate('CASCADE');
@@ -36,7 +37,13 @@ return new class extends Migration
     public function down()
     {
         Schema::table('businesses', function (Blueprint $table) {
-            //
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            $table->dropColumn('region_id');
+            $table->dropColumn('province_id');
+            $table->dropColumn('district_id');
+            $table->dropColumn('city_id');
+            $table->dropColumn('group_id');
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         });
     }
 };
