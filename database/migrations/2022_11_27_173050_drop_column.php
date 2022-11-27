d7cce2df24f483c21c476f654b36795c0f149137
Sujan Pradhan <niweshs@gmail.com>
2022-11-27 23:41:14 +0545

    migration done

--- database/migrations/2022_11_27_173050_drop_column.php
+++ database/migrations/2022_11_27_173050_drop_column.php
@@ -18,30 +18,35 @@ return new class extends Migration
         if (Schema::hasColumn('businesses', 'region_id'))
         {
             Schema::table('businesses', function (Blueprint $table) {
+                $table->dropForeign('businesses_region_id_foreign');
                 $table->dropColumn('region_id');
             });
         }
         if (Schema::hasColumn('businesses', 'province_id'))
         {
             Schema::table('businesses', function (Blueprint $table) {
+                $table->dropForeign('businesses_province_id_foreign');
                 $table->dropColumn('province_id');
             });
         }
         if (Schema::hasColumn('businesses', 'district_id'))
         {
             Schema::table('businesses', function (Blueprint $table) {
+                $table->dropForeign('businesses_district_id_foreign');
                 $table->dropColumn('district_id');
             });
         }
         if (Schema::hasColumn('businesses', 'city_id'))
         {
             Schema::table('businesses', function (Blueprint $table) {
+                $table->dropForeign('businesses_city_id_foreign');
                 $table->dropColumn('city_id');
             });
         }
         if (Schema::hasColumn('businesses', 'group_id'))
         {
             Schema::table('businesses', function (Blueprint $table) {
+                $table->dropForeign('businesses_group_id_foreign');
                 $table->dropColumn('group_id');
             });
         }
